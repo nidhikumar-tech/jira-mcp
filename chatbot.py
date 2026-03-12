d32b98adbd8c7dccf537b1bca32254cdc77662a1
nidhikumar <nidhi.kumar@onixnet.com>
2026-03-12 13:27:08 +0530

    Added prompts for the chatbot to return cleaned data

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,7 @@
 import asyncio
 import os
 from dotenv import load_dotenv
+from langchain.messages import SystemMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_mcp_adapters.client import MultiServerMCPClient
 from langgraph.prebuilt import create_react_agent 
@@ -32,9 +33,16 @@ async def run_chatbot():
     # 3. Fetch tools
     print("Connecting to MCP Servers...")
     tools = await mcp_client.get_tools()
+
+    system_instructions = """
+    Your job is to fetch information from JIRA.
+    After processing the user's request, and getting an answer from the tool, you should respond with a concise answer to the user.
+    If an epic has been asked for, return the epic's name, key, creator, and description. If a list of issues is returned, return the issue keys and summaries, not just the keys.
+    Do not ever return the raw tool response to the user, always process it and return a concise answer.
+    """
     
     # 4. Create the LangGraph agent
-    agent_executor = create_react_agent(llm, tools)
+    agent_executor = create_react_agent(llm, tools, prompt=SystemMessage(content=system_instructions))
 
     print("--- JIRA Chatbot Ready ---")
     while True:
